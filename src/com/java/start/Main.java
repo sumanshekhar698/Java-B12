8995c93674e575119b4482c8d1e12060a16b8516
Suman Shekhar <suman.sumanshekhar698@gmail.com>
2024-05-07 10:33:42 +0530

    first commit

---
+++ src/com/java/start/Main.java
@@ -0,0 +1,21 @@
+package com.java.start; //R1c package declaration is Must
+
+//R2 class name should EXACTLY match the File Name
+class Main {// Source Code ---> ByteCode
+//	C2 class name shoukld be in PascalCase
+//	   TomAndJerry
+
+	/*
+	 * C1: package name should be small case google.com com.google.android
+	 */
+
+//	R1
+
+	public static void main(String... suman) {// R3 keep tha min fn as it is
+		System.out.print("Hello World");
+//		C2 : 
+		int valueOfADozen= 12;//C3 variable name and fn/methods should be in camelCase
+		//R4 variable name can be of length, it should not start with a number, you can also use $ and _
+	}
+
+}
